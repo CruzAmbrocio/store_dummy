9ca901e7ef0828409bd4697a2d04be68ee24fbe3
CruzAmbrocio <cruzambrocio1409@gmail.com>
2023-11-24 23:35:30 -0600

    Product details screen

---
+++ src/screens/ProductDetails.js
@@ -0,0 +1,76 @@
+import { FlatList, Image, Pressable, ScrollView, StyleSheet, Text, View, useWindowDimensions } from "react-native";
+import products from "../data/products";
+
+const ProductDetails=()=>{
+  const product = products[0];
+  const {width} = useWindowDimensions();
+  const addToCart=()=>{
+    console.warn('add to cart')
+  }
+  return(
+    <View>
+    <ScrollView>
+      <FlatList
+        key={'_'}
+        keyExtractor={item => "_" + item}
+        data={product.images}
+        renderItem={({item})=>(
+            <Image source={{uri:item}} style={{
+              width:width,
+              aspectRatio:1}}
+            />
+        )}
+        horizontal
+        numColumns={1}
+        showsHorizontalScrollIndicator={false}
+        pagingEnabled
+      />
+      <View style={{ padding: 20 }}>
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+        <Text style={styles.description}>{product.description}</Text>
+      </View>
+    </ScrollView>
+    <Pressable onPress={addToCart} style={styles.button}>
+      <Text style={styles.buttonTex}>Add to cart</Text>
+    </Pressable>
+
+
+    </View>
+
+  );
+}
+const styles = StyleSheet.create({
+  title:{
+    fontSize:34,
+    fontWeight:'bold',
+    marginVertical:10
+  },
+  price:{
+    fontWeight:'500',
+    fontSize:16
+  },
+  description:{
+    marginVertical:10,
+    fontSize:18,
+    lineHeight:30,
+    fontWeight:'300'
+  },
+  button:{
+    position:'absolute',
+    backgroundColor:'black',
+    bottom:30,
+    width:'90%',
+    alignSelf:'center',
+    padding:20,
+    borderRadius:100,
+    alignItems:'center'
+  },
+  buttonTex:{
+    color:'white',
+    fontWeight:'500',
+    fontSize:16 
+  }
+});
+
+export default ProductDetails;
